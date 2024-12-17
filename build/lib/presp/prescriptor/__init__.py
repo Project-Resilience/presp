3e6fc0e00bbbc7a4a8e9543a2777c45daf9d84f0
Daniel Young <danyoung@utexas.edu>
2024-12-17 10:10:51 -0800

    Removed build from branch

--- build/lib/presp/prescriptor/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Initializes modules in the prescriptor subpackage.
-"""
-from presp.prescriptor.base import Prescriptor, PrescriptorFactory
-
-from presp.prescriptor.nn import NNPrescriptor, NNPrescriptorFactory
